000a4c4148359cf2d84be718a7e73c123db73301
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-08-17 23:10:58 +0200

    Implement AST printer

---
+++ src/main/kotlin/lox/AstPrinter.kt
@@ -0,0 +1,29 @@
+package lox
+
+class AstPrinter : Visitor<String> {
+    fun print(expr: Expr) = expr.accept(this)
+
+    override fun visitBinaryExpr(expr: Binary): String {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    }
+
+    override fun visitGroupingExpr(expr: Grouping): String {
+        return parenthesize("group", expr.expression)
+    }
+
+    override fun visitLiteralExpr(expr: Literal): String {
+        return expr.value?.toString() ?: "nil"
+    }
+
+    override fun visitUnaryExpr(expr: Unary): String {
+        return parenthesize(expr.operator.lexeme, expr.right)
+    }
+
+    private fun parenthesize(name: String, vararg exprs: Expr) = buildString {
+        append("($name")
+        for (expr in exprs) {
+            append(" ${expr.accept(this@AstPrinter)}")
+        }
+        append(")")
+    }
+}
---
+++ src/test/kotlin/lox/AstPrinterTest.kt
@@ -0,0 +1,20 @@
+package lox
+
+import lox.TokenType.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AstPrinterTest {
+    @Test
+    fun `print without parsing`() {
+        val expression = Binary(
+            Unary(
+                Token(MINUS, "-", null, 1),
+                Literal(123),
+            ),
+            Token(STAR, "*", null, 1),
+            Grouping(Literal(45.67))
+        )
+        assertEquals("(* (- 123) (group 45.67))", AstPrinter().print(expression))
+    }
+}
